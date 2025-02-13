3b23f46c4bf43597eaa5bd1a924b6b047045407c
Alexandre Araujo <80693423+Alexandre-CA@users.noreply.github.com>
2025-02-13 23:45:18 +0000

    status API

---
+++ src/app/api/v1/status/index.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function status(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ message: 'Hello from Next.js!' })
+}
