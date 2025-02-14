5cffebf59138e47a27115e043d7142996b662773
Alexandre Araujo <80693423+Alexandre-CA@users.noreply.github.com>
2025-02-14 00:08:56 +0000

    and

--- src/app/api/v1/status/index.ts
+++ src/app/api/v1/status/index.ts
@@ -1,8 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import database from "@/infra/database";
 
-export default function status(
+export default async function status(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   res.status(200).json({ message: 'Status' })
 }
