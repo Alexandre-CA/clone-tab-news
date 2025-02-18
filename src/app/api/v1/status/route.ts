170da202bc694a1c99e722e1ece340d6497ff0d4
alexandre <alexandre220404@gmail.com>
2025-02-17 21:14:48 -0300

    finish route /status

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -4,10 +4,31 @@ import { NextResponse } from 'next/server';
 
 // GET request handler
 export async function GET() {
-  const query = await database.query("SELECT 1 + 1 AS count")
-  return NextResponse.json({
-    query: query.rows
-  }, {
+
+  const updatedAt = new Date().toISOString()
+
+  // Query the database for ther version
+  const dbVersion = (await database.query("SHOW server_version")).rows[0];
+
+  // Query the database for the max connections
+  const dbMaxConnection = (await database.query("SHOW max_connections")).rows[0];
+
+  // Query the database for the used connections
+  const usedConnections = (await database.query("SELECT COUNT(*)::int FROM pg_stat_activity WHERE state='active';")).rows[0];
+
+  const body = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.server_version,
+        max_connections:  parseInt(dbMaxConnection.max_connections),
+        opened_connections: usedConnections.count
+      }
+    }
+
+  }
+
+  return NextResponse.json(body, {
     status: 200
   });
 }
--- src/test/status/get.test.ts
+++ src/test/status/get.test.ts
@@ -1,1 +1,25 @@
-test("")
+
+it("GET to /api/v1/status should return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+
+    expect(response.status).toBe(200);
+});
+
+it("GET to /api/v1/status should correct Body", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+
+    const responseBody = await response.json();
+
+    const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+
+    expect(responseBody).toEqual({
+        updated_at: parsedUpdateAt,
+        dependencies: {
+            database: {
+                version: expect.any(String),
+                max_connections: expect.any(Number),
+                opened_connections: 1,
+            }
+        }
+    })
+})
