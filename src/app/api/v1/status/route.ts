d9fcbb4759d2aa13d3498b920c41ed9d1d5964d3
alexandre <alexandre220404@gmail.com>
2025-03-18 19:37:07 -0300

    feat: add type assertions for database query results in status route

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -8,13 +8,13 @@ export async function GET() {
   const updatedAt = new Date().toISOString()
 
   // Query the database for ther version
-  const dbVersion = (await database.query("SHOW server_version")).rows[0];
+  const dbVersion = (await database.query("SHOW server_version")).rows[0] as { server_version: string };
 
   // Query the database for the max connections
-  const dbMaxConnection = (await database.query("SHOW max_connections")).rows[0];
+  const dbMaxConnection = (await database.query("SHOW max_connections")).rows[0] as { max_connections: string };
 
   // Query the database for the used connections
-  const usedConnections = (await database.query("SELECT COUNT(*)::int FROM pg_stat_activity WHERE state='active';")).rows[0];
+  const usedConnections = (await database.query("SELECT COUNT(*)::int FROM pg_stat_activity WHERE state='active';")).rows[0] as { count: number };
 
   const body = {
     updated_at: updatedAt,
