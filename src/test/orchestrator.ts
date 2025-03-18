e3c2002b14e715e725188cb1c83085f1fce7fe07
alexandre <alexandre220404@gmail.com>
2025-03-18 19:24:12 -0300

    feat: add GitHub Actions workflow for automated testing and update wait-for-postgres script

--- src/test/orchestrator.ts
+++ src/test/orchestrator.ts
@@ -4,13 +4,16 @@ const waitForAllServices = async () => {
     const waitForWebServer = async () => {
         const fetchStatusPage = async () => {
             const response = await fetch("http://localhost:3000/api/v1/status");
-            await response.json();
+
+            if (response.status !== 200) {
+                throw Error( );
+            }
 
         }
 
         return retry(fetchStatusPage, {
             retries: 100,
-            maxTimeout:5000
+            maxTimeout: 5000
         })
     }
     await waitForWebServer();
